78481293fe8b7960b11527cc090671a8a68b834b
Shrey Lawang <slawang108@gmail.com>
2022-01-14 11:25:45 +0530

    Solution Task#2 v2

--- tasks/task2/assets/script.js
+++ tasks/task2/assets/script.js
@@ -4,15 +4,18 @@ function verify() {
   let tempVar = userInput.value;
   if (isNaN(tempVar)) {
     userInput.classList.add("invalidInput");
+    result.classList.add("invalid");
     result.innerText = "Invalid Input";
     return;
   } else if (tempVar % 2 == 0) {
     // userInput.classList.add("invalid");
     result.innerText = `${tempVar} is Even`;
     userInput.classList.remove("invalidInput");
+    result.classList.remove("invalid");
   } else {
     // userInput.classList.add("invalid");
     result.innerText = `${tempVar} is Odd`;
     userInput.classList.remove("invalidInput");
+    result.classList.remove("invalid");
   }
 }
